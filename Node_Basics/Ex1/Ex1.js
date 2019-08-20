d344e28a27a7483ecd8b66c9ca4759663d067fb4
Jack Harding <32617958+jacKlinc@users.noreply.github.com>
2019-08-20 15:05:29 +0100

    Update Ex1.js

--- Node_Basics/Ex1/Ex1.js
+++ Node_Basics/Ex1/Ex1.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 
 fs.readFile("index.html", function (err, data) {
-<<<<<<< HEAD
     if (err) console.log(err); // returns error 
     else console.log(data.toString());	// outputs contents of file	
 });
@@ -12,9 +11,3 @@ fs.readFile("index.html", function (err, data) {
     if (err) console.log(err);
     else console.log(data.toString());		
 });
-/*
->>>>>>> 0297b3b78f33ce91f04fa7a7fe7d6855aba3631a
-{ [Error: ENOENT: no such file or directory, 
-open 'index.html'] errno: -2, code: 'ENOENT', 
-syscall: 'open', path: 'index.html' }
-*/
